939c0b71fd8c658912acb2435f0d842e47493adf
vedprakash2017 <vedprakashtawar2016@gmail.com>
2021-05-07 12:18:29 +0530

    audio

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -5,23 +5,53 @@ import Fade from 'react-reveal/Fade';
 export class DashboardPage extends React.Component {
         state = {
           left: 0,
-          top: 0
+          top: 0,
+          play: false,
+          audio : new Audio("https://edolve.s3.ap-south-1.amazonaws.com/1620296923Adventure Trailer.mp3")
+      
         }
       
         componentDidMount() {
           // When the component is mounted, add your DOM listener.
-          document.addEventListener("mousemove", this.handleCursor);
+        document.addEventListener("mousemove", this.handleCursor);
+        this.state.audio.addEventListener('ended', () => this.setState({ play: true }));
+        if(this.state.play == true)
+        {
+        this.state.audio.play();
+        // this.state.play = false;
+        this.state.audio.loop = true;  
+        this.state.audio.volume = 0.3;
+        // this.togglePlay();
         }
-      
+    }
+        componentDidUpdate(){
+            if(this.state.play == true)
+        {this.state.audio.play();
+        this.state.audio.loop = true;  
+        this.state.audio.volume = 0.3;
+        }
+    }
         componentWillUnmount() {
           // Make sure to remove the DOM listener when the component is unmounted.
           document.removeEventListener("mousemove", this.handleCursor);
+
+        this.state.audio.removeEventListener('ended', () => this.setState({ play: false }));
+        this.state.audio.pause();
+        this.state.play = false;
+
         }
-      
+            togglePlay = () => {
+        this.setState({ play: !this.state.play }, () => {
+          this.state.play ? this.state.audio.play() : this.state.audio.pause();
+          this.state.audio.loop = true;
+        this.state.audio.volume = 0.3;
+        });
+      }
+
         handleCursor = (e) => {
           const card = document.getElementsByClassName('card')[0];     
         //   cursor.setAttribute('style','top:'+e.pageY+'px; left:'+ e.pageX+'px;');
-
+            if(this.state.play == false) { this.setState({ play: true });}
 
           var ax = -($(window).innerWidth()/2- e.pageX)/20;
           var ay = ($(window).innerHeight()/2- e.pageY)/10;
@@ -45,6 +75,8 @@ export class DashboardPage extends React.Component {
 
 
 <div style={{background:"white"}}>
+    {/* {  this.state.play ? <i class="fa fa-volume-up fa-3x" onClick={this.togglePlay} aria-hidden="true"></i> : <i onClick={this.togglePlay} class="fa fa-volume-mute fa-3x" aria-hidden="true"></i>} */}
+    {/* <button onClick={this.togglePlay} > ON </button> */}
     <div class="main" >
         <div class="imgi"> </div> 
         <div class="card" style={{height:"500px",width:"475px"}}> 
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,33 @@ import { resetType } from '../actions/clientType';
 
 export class Header extends React.Component {
     
+    state = {
+        play: false,
+        audio : new Audio("https://edolve.s3.ap-south-1.amazonaws.com/1620131058home.wav")
+      }
+      // audio = new Audio("https://edolve.s3.ap-south-1.amazonaws.com/1620131058home.wav")
+    
+      componentDidMount() {
+        this.state.audio.addEventListener('ended', () => this.setState({ play: true }));
+        this.state.audio.play();
+        this.state.audio.loop = true;  
+        this.state.audio.volume = 0.1;
+      }
+    
+      componentWillUnmount() {
+        this.state.audio.removeEventListener('ended', () => this.setState({ play: false }));
+        this.state.audio.pause();
+    }
+    
+      togglePlay = () => {
+        this.setState({ play: !this.state.play }, () => {
+          this.state.play ? this.state.audio.play() : this.state.audio.pause();
+          this.state.audio.loop = true;
+        this.state.audio.volume = 0.1;
+        });
+      }
+
+
     handleClick = () => {
         socket.disconnect();
         socket.connect();
@@ -24,6 +51,7 @@ export class Header extends React.Component {
                     <div className={"header__content"}>
                         <button className={"header__title button--link button"} onClick={this.handleClick}><h1>OpenTrivia</h1></button>
                         <button className="button--link button ">About</button>
+                        <button onClick={this.togglePlay} style={{display:"none"}}>{this.state.play ? 'Pause' : 'Play'}</button>
                     </div>
                 </div>
             </header>
